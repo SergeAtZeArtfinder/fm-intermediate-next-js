d92f3391ac359cb92558fd535d48babb5e73f0fb
Serge Basangovs <sergejs.basangovs@artfinder.com>
2025-01-15 11:11:47 +0000

    feat: added initial auth UI

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from '@/components/SignUpForm'
+
+const SignUpPage = async () => {
+  return (
+    <>
+      <SignUpForm />
+    </>
+  )
+}
+
+export default SignUpPage
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+const DashboardPage = async () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Dashboard Page
+      </h1>
+    </>
+  )
+}
+
+export default DashboardPage
