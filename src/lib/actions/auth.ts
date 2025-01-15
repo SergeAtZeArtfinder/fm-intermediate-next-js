2399e353fe64d8037ab036403f6f0e95ce156f47
Serge Basangovs <sergejs.basangovs@artfinder.com>
2025-01-15 10:54:49 +0000

    feat: auth server actions added

---
+++ src/lib/actions/auth.ts
@@ -0,0 +1,45 @@
+'use server'
+
+import { redirect } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { z } from 'zod'
+
+import { COOKIE_NAME } from '@/constants'
+import { signin, signup } from '../authTools'
+
+const authSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+export const registerUser = async <S>(prevState: S, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signup(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you up' }
+  }
+  redirect('/dashboard')
+}
+
+export const signinUser = async <S>(prevState: S, formData: FormData) => {
+  const data = authSchema.parse({
+    email: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  try {
+    const { token } = await signin(data)
+    cookies().set(COOKIE_NAME, token)
+  } catch (e) {
+    console.error(e)
+    return { message: 'Failed to sign you in' }
+  }
+  redirect('/dashboard')
+}
